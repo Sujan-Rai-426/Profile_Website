8279e10722f018efcde628ea3854970344460b8c
Sujan-Rai-426 <sujanrai20070140@gmail.com>
2024-11-29 13:00:23 +0545

    edited Profile/settings.py [ Edit:- new if else codition using debug for accesing static files ]

--- Profile/settings.py
+++ Profile/settings.py
@@ -149,25 +149,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# Manually added apps static file
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-# Manually added apps media file
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC_URL = 'static/'
+# # Manually added apps static file
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# # Manually added apps media file
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# if DEBUG:
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  #Aditional static files for development 
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Directory for static files in production
 
-# MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]  # Directory for uploaded media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
