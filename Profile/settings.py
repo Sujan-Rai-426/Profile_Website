5a539cf289da5af4b8febdcc46b09640323f1ea4
Sujan <rsujan140.in@gmail.com>
2024-12-01 15:20:24 +0545

    restored

--- Profile/settings.py
+++ Profile/settings.py
@@ -41,7 +41,7 @@ if DEBUG:
     EMAIL_HOST_PASSWORD = '8dfdbdbd700e2d'
     EMAIL_PORT = '2525'
 else:   
-    EMAIL_BACKEND = "djnago.core.mail.backends.smtp.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
